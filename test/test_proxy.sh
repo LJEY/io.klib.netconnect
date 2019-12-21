adb4ea337de5598d26e1557c7f6cee3cf20b6a3a
LJEY <lukas.joraschek@gmail.com>
2019-12-21 15:43:38 +0100

    added dev branch

--- test/test_proxy.sh
+++ test/test_proxy.sh
@@ -3,6 +3,7 @@
 #set -u
 #set -e
 
+
 function proxyCheck {
     printf "%s\n" "Checking if a proxy for $1 is already defined on os levl"
     eval var='$'$1_proxy
@@ -16,170 +17,116 @@ function proxyCheck {
     fi
 }
 
-proxyCheck http
-proxyCheck https
+#proxyCheck http
+#proxyCheck https
 
-exit 0 
+#JAVA_VMARG="-Dhttp.proxyHost=127.0.0.1 -Dhttp.proxyPort=8080"
+
+export http_proxy=127.0.0.1:8080
+export https_proxy=127.0.0.1:8080
+
+java -jar ./../build/libs/io.klib.netconnect-0.1.0-SNAPSHOT.jar https://www.heise.de > output.log 2> error.log
+
+        if [ $? == 0 ]
+        then
+            printf "%s\n" "     Success, connection per http works"
+        else
+            printf "%s\n" "     Error, connection for http didn't work"
+            exit 1
+        fi
+
+exit 0
+            
+function connectionTest {
     
-printf "%s\n" "Testing direct connection for http"
-
-    java -jar /home/lukas/klib/io.klib.netconnect/build/libs/io.klib.netconnect-0.1.0-SNAPSHOT.jar http://www.columbia.edu/~fdc/sample.html > output.log 2> error.log
-
-    if [ $? == 0 ]
+    
+    if [ $1 = http ]
     then
-        printf "%s\n" "     Success, direct connection per http works"
-    else
-        printf "%s\n" "     Error, direct connection for http didn't work"
-        exit 1
-    fi
+    
+        printf "%s\n" "Testing connection for http"
+    
+        java ${JAVA_VMARGS} -jar ./../build/libs/io.klib.netconnect-0.1.0-SNAPSHOT.jar http://www.columbia.edu/~fdc/sample.html > output.log 2> error.log
 
-printf "%s\n" "Testing faulty direct connection for http"
-
-    java -jar /home/lukas/klib/io.klib.netconnect/build/libs/io.klib.netconnect-0.1.0-SNAPSHOT.jar http://www.columba.edu/~fdc/sample.html > output.log 2> error.log
-
-    if [ $? == 1 ]
-    then
-        printf "%s\n" "     Success, system exit code as expected"
-    else
-        printf "%s\n" "     Error, system exit code not as expected"
-        exit 1
-    fi
-
-printf "%s\n" "Testing direct connection for https"
-
-    java -jar /home/lukas/klib/io.klib.netconnect/build/libs/io.klib.netconnect-0.1.0-SNAPSHOT.jar https://www.heise.de > output.log 2> error.log
-
-    if [ $? == 0 ]
-    then
-        printf "%s\n" "     Success, direct connection per https works"
-    else
-        printf "%s\n" "     Error, direct connection for https didn't work"
-        exit 1
-    fi
-
-printf "%s\n" "Testing faulty direct connection for https"
+        if [ $? == 0 ]
+        then
+            printf "%s\n" "     Success, connection per http works"
+        else
+            printf "%s\n" "     Error, connection for http didn't work"
+            exit 1
+        fi
         
-    java -jar /home/lukas/klib/io.klib.netconnect/build/libs/io.klib.netconnect-0.1.0-SNAPSHOT.jar https://www.gooogle.de > output.log 2> error.log
+        printf "%s\n" "Testing faulty connection for http"
+        
+        java ${JAVA_VMARGS} -jar ./../build/libs/io.klib.netconnect-0.1.0-SNAPSHOT.jar http://www.columba.edu/~fdc/sample.html > output.log 2> error.log
 
-    if [ $? == 1 ]
-    then
-        printf "%s\n" "     Success, system exit code as expected"
-    else
-        printf "%s\n" "     Error, system exit code not as expected"
-        exit 1
+        if [ $? == 1 ]
+        then
+            printf "%s\n" "     Success, system exit code as expected"
+        else
+            printf "%s\n" "     Error, system exit code not as expected"
+            exit 1
+        fi
     fi
+    if [ $1 = https ]
+    then
+        
+        printf "%s\n" "Testing connection for https"
+    
+        java ${JAVA_VMARGS} -jar ./../build/libs/io.klib.netconnect-0.1.0-SNAPSHOT.jar https://www.heise.de > output.log 2> error.log
+
+        if [ $? == 0 ]
+        then
+            printf "%s\n" "     Success, connection per https works"
+        else
+            printf "%s\n" "     Error, connection for https didn't work"
+            exit 1
+        fi
+        
+        printf "%s\n" "Testing faulty connection for https"
+        
+        java ${JAVA_VMARGS} -jar /home/lukas/klib/io.klib.netconnect/build/libs/io.klib.netconnect-0.1.0-SNAPSHOT.jar https://www.gooogle.de > output.log 2> error.log
+
+        if [ $? == 1 ]
+        then
+            printf "%s\n" "     Success, system exit code as expected"
+        else
+            printf "%s\n" "     Error, system exit code not as expected"
+            exit 1
+        fi
+    fi
+}
+ 
+printf "%s\n" "Testing direct connection" 
+ 
+connectionTest http 
+connectionTest https 
 
 printf "%s\n" "Starting a nginx proxy server in a docker container"
 
-sudo docker run --name temp -d -p 8080:8080 klibio/io.klib.forwarding-ssl-proxy:master-latest
+#sudo docker run --name temp -d -p 8080:8080 klibio/io.klib.forwarding-ssl-proxy:master-latest
 
 printf "%s\n" "Configuring proxy for java"
 
-    JAVA_VMARG="-Dhttp.proxyHost=<127.0.0.1> -Dhttp.proxyPort=<8080>"
+JAVA_VMARG="-Dhttp.proxyHost=<127.0.0.1> -Dhttp.proxyPort=<8080>"
 
-printf "%s\n" "Testing java proxy connection for http"
-
-    java ${JAVA_VMARGS} -jar /home/lukas/klib/io.klib.netconnect/build/libs/io.klib.netconnect-0.1.0-SNAPSHOT.jar http://www.columbia.edu/~fdc/sample.html > output.log 2> error.log
-
-    if [ $? == 0 ]
-    then
-        printf "%s\n" "     Success, java proxy connection per http works"
-    else
-        printf "%s\n" "     Error, java proxy connection per http didn't work"
-        exit 1
-    fi
-
-printf "%s\n" "Testing faulty java proxy connection for http"    
+printf "%s\n" "Testing connection with java proxy"  
     
-    java ${JAVA_VMARGS} -jar /home/lukas/klib/io.klib.netconnect/build/libs/io.klib.netconnect-0.1.0-SNAPSHOT.jar http://www.columba.edu/~fdc/sample.html > output.log 2> error.log
-
-    if [ $? == 1 ]
-    then
-        printf "%s\n" "     Success, exit code as expected"
-    else
-        printf "%s\n" "     Error, exit code not as expected"
-        exit 1
-    fi
-    
-printf "%s\n" "Testing java proxy connection for https"
-
-    java ${JAVA_VMARGS} -jar /home/lukas/klib/io.klib.netconnect/build/libs/io.klib.netconnect-0.1.0-SNAPSHOT.jar https://www.heise.de > output.log 2> error.log
-
-    if [ $? == 0 ]
-    then
-        printf "%s\n" "     Success, java proxy connection per https works"
-    else
-        printf "%s\n" "     Error, java proxy connection per https didn't work"
-        exit 1
-    fi
-
-printf "%s\n" "Testing faulty java proxy connection for https" 
-    
-    java ${JAVA_VMARGS} -jar /home/lukas/klib/io.klib.netconnect/build/libs/io.klib.netconnect-0.1.0-SNAPSHOT.jar https://www.gooogle.de > output.log 2> error.log
-
-    if [ $? == 1 ]
-    then
-        printf "%s\n" "     Success, exit code as expected"
-    else
-        printf "%s\n" "     Error, exit code not as expected"
-        exit 1
-    fi
+connectionTest http 
+connectionTest https 
 
 printf "%s\n" "Setting os wide proxy"
 
-    export http_proxy=http://127.0.0.1:8080
-    export https_proxy=http://127.0.0.1:8080
+    export http_proxy=127.0.0.1:8080
+    export https_proxy=127.0.0.1:8080
 
-printf "%s\n" "Testing os proxy connection for http"
+printf "%s\n" "Testing connection with os wide proxy"  
 
-    java -jar /home/lukas/klib/io.klib.netconnect/build/libs/io.klib.netconnect-0.1.0-SNAPSHOT.jar http://www.columbia.edu/~fdc/sample.html > output.log 2> error.log
-
-    if [ $? == 0 ]
-    then
-        printf "%s\n" "     Success, os proxy connection per http works"
-    else
-        printf "%s\n" "     Error, os proxy connection per http didn't work"
-        exit 1
-    fi
-
-printf "%s\n" "Testing faulty os proxy connection for http"
-
-    java -jar /home/lukas/klib/io.klib.netconnect/build/libs/io.klib.netconnect-0.1.0-SNAPSHOT.jar http://www.columba.edu/~fdc/sample.html > output.log 2> error.log
-
-    if [ $? == 1 ]
-    then
-        printf "%s\n" "     Success, exit code as expected"
-    else
-        printf "%s\n" "     Error, exit code not as expected"
-        exit 1
-    fi
-
-printf "%s\n" "Testing os proxy connection for https"
-
-    java -jar /home/lukas/klib/io.klib.netconnect/build/libs/io.klib.netconnect-0.1.0-SNAPSHOT.jar https://www.heise.de > output.log 2> error.log
-
-    if [ $? == 0 ]
-    then
-        printf "%s\n" "     Success, os proxy connection per https works"
-    else
-        printf "%s\n" "     Error, os proxy connection per https didn't work"
-        exit 1
-    fi
-
-printf "%s\n" "Testing faulty os proxy connection for https"
-    
-    java -jar /home/lukas/klib/io.klib.netconnect/build/libs/io.klib.netconnect-0.1.0-SNAPSHOT.jar https://www.gooogle.de > output.log 2> error.log
-
-    if [ $? == 1 ]
-    then
-        printf "%s\n" "     Success, exit code as expected"
-    else
-        printf "%s\n" "     Error, exit code not as expected"
-        exit 1
-    fi
+connectionTest http 
+connectionTest https 
 
 printf "%s\n" "Removing docker container again"
 
-    sudo docker rm -f temp
+#    sudo docker rm -f temp
 
 printf "%s\n" "Nice, everything works as intended!" 
+
